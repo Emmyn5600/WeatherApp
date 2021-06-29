bedc6288b7e71eeb061a6913e8ccb222921a0325
emmyn5600 <emmy6618@gmail.com>
2021-06-29 12:39:03 +0200

    Use axios to fetch the api informatiom

---
+++ src/components/_cloud.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import {cityWeatherInfo} from '../helpers_api/open'
+export default class Weather {
+    async find(city, displayLoader) {
+      displayLoader();
+      const { data } = await axios.get(cityWeatherInfo(city));
+      this.weatherInfo = data;
+      return this;
+    }
+  
+    get() {
+      return this.weatherInfo;
+    }
+  }
